18f89fde9589e14395bf37213e29a41915627698
jcdiem <jcdiem@protonmail.com>
2021-05-31 21:54:38 -0500

    SSH Functions

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 //Global Space
 const path = require('path')
-const {BrowserWindow } = require('electron')
+const {app, BrowserWindow } = require('electron')
 const {readFileSync } = require('fs');
 const {Client } = require('ssh2');
 
-function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('UI/html/testScreen.html')
-}
-
-
-app.whenReady().then(() => {
-    //Runtime
-    
-    /**
+/**
      * Create an OpenSSH client connection to the server
      * @param {String} priv The path of the private key of the user
      * @param {String} user The user to connect as
@@ -25,30 +12,61 @@ app.whenReady().then(() => {
      * @param {number} port Port number to connect to the server on
      * @returns {Client} An open SSH connection to the server
      */
-    function createSSH(priv,user,host,port){
-        conn = new Client();
-        conn.connect({
-            host: host,
-            port: port,
-            username: user,
-            privateKey: readFileSync(priv)
-        });
+ function createSSH(priv,user,host,port){
+    conn = new Client();
+    conn.connect({
+        host: host,
+        port: port,
+        username: user,
+        privateKey: readFileSync(priv)
+    });
 
-        //Assume connection is bad
-        goodConnect = false;
+    //Assume connection is bad
+    goodConnect = false;
 
-        conn.on('ready', () =>{
-            console.debug('SSH connection established, attempting test command.');
-            conn.exec('uptime')
-        });
-        
-        if(goodConnect) return conn;
-        else {
-            console.error('SSH connection failed!');
-            return null;
-        }
+    conn.on('ready', () =>{
+        console.debug('SSH connection established, attempting test command.');
+        if(sshCmd(conn,'whoami') != null) goodConnect = true;
+    });
+
+    if(goodConnect) return conn;
+    else {
+        console.error('SSH connection failed!');
+        return null;
     }
+}
+
+/**
+ * Run a command in a server connected via SSH
+ * @param {Client} conn The SSH connection to use
+ * @param {String} cmd The command to be run on the server
+ * @returns {object} STDOUT and STDERR in strings
+ */
+function sshCmd(conn,cmd){
+    cmdOutput = {err: "", out: ""};
+    conn.exec(cmd, (err, stream) => {            
+        if (err) throw err;
+        //Once done receiving data
+        stream.on('close', (code, signal) => {
+            console.debug('Stream :: close :: code: ' + code + ', signal: ' + signal);
+            conn.end();                
+        }).on('data', (stdout) =>{ //Get the STDOUT
+            cmdOutput.out = stdout;
+        }).stderr.on('data', (stderr) =>{ //Get the STDERR
+            cmdOutput.err = stderr;
+        });
+    });
+    return cmdOutput;
+}
 
 
-    createWindow();
+
+app.whenReady().then(() => {
+    //Runtime
+
+    //Create window and load main menu
+    win = new BrowserWindow({width:800,height:600,title:'Linux GSM Manager',titleBarStyle:'hidden'});
+    //win.loadfile('main menu');
+    win.loadFile('UI/html/testScreen.html');
+
 })
