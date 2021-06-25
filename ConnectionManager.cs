f03d34b083c3662379ce6b7651e5beb1aa6008f2
JLP <jcdiem@protonmail.com>
2021-06-25 15:13:11 -0500

    C# Setup

---
+++ ConnectionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Renci.SshNet;
+
+namespace lgsm_mngr
+{
+    class ConnectionManager {
+        SshClient shClient; //SSH Client
+        SftpClient sfClient; //SFTP Client
+        ConnectionInfo conInfo; //Protocol agnostic connection details
+
+        public ConnectionManager(string host, string username, int port, string privKeyFile) {
+            ConnectionInfo conInfo = new ConnectionInfo(
+                host,
+                port,
+                username,
+                new PrivateKeyAuthenticationMethod("user-data/"+privKeyFile)
+                );
+        }
+
+        public void connectSSH() {
+            using (shClient = new SshClient(conInfo)) {
+                shClient.Connect();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace lgsm_mngr {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Loading debug info...");
+        }
+    }
+}
